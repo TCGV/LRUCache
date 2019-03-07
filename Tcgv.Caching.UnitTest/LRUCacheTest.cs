d4a937a5ea95afa5e72daeb30dc28dc7e1a9adac
Thomas Vilhena <thomas.vilhena@outlook.com>
2019-03-07 13:21:33 -0300

    Initial commit

---
+++ Tcgv.Caching.UnitTest/LRUCacheTest.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tcgv.Caching.UnitTest
+{
+    [TestClass]
+    public class LRUCacheTest
+    {
+        [TestMethod]
+        public void GetNullItem_Test()
+        {
+            var cache = new LRUCache<string, string>(int.MaxValue);
+
+            var value = cache.Get("key");
+
+            Assert.IsNull(value);
+        }
+
+        [TestMethod]
+        public void SetAndGetItem_Test()
+        {
+            var cache = new LRUCache<string, string>(int.MaxValue);
+
+            cache.Set("key", "1234");
+            var value = cache.Get("key");
+
+            Assert.AreEqual("1234", value);
+        }
+
+        [TestMethod]
+        public void SetItemTwice_Test()
+        {
+            var cache = new LRUCache<string, string>(int.MaxValue);
+
+            cache.Set("key", "1234");
+            cache.Set("key", "5678");
+            var value = cache.Get("key");
+
+            Assert.AreEqual("5678", value);
+        }
+
+        [TestMethod]
+        public void Eviction_OnlySettingItems_Test()
+        {
+            var cache = new LRUCache<string, string>(3);
+
+            cache.Set("one", "1");
+            cache.Set("two", "2");
+            cache.Set("three", "3");
+            cache.Set("four", "4");
+
+            Assert.IsNull(cache.Get("one"));
+            Assert.AreEqual("2", cache.Get("two"));
+            Assert.AreEqual("3", cache.Get("three"));
+            Assert.AreEqual("4", cache.Get("four"));
+        }
+
+        [TestMethod]
+        public void Eviction_WhenGettingItems_Test()
+        {
+            var cache = new LRUCache<string, string>(3);
+
+            cache.Set("one", "1");
+            cache.Set("two", "2");
+            cache.Set("three", "3");
+            cache.Get("one");
+            cache.Set("four", "4");
+
+            Assert.AreEqual("1", cache.Get("one"));
+            Assert.IsNull(cache.Get("two"));
+            Assert.AreEqual("3", cache.Get("three"));
+            Assert.AreEqual("4", cache.Get("four"));
+        }
+    }
+}
---
+++ Tcgv.Caching/LRUCache.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace Tcgv.Caching
+{
+    public class LRUCache<TKey, TValue>
+    {
+        public LRUCache(int size)
+        {
+            this.size = size;
+            this.accesses = new LinkedList<KeyValuePair<TKey, TValue>>();
+            this.cache = new Dictionary<TKey, LinkedListNode<KeyValuePair<TKey, TValue>>>();
+        }
+
+        public TValue Get(TKey key)
+        {
+            if (cache.ContainsKey(key))
+            {
+                SetMostRecentlyAccessed(key);
+                return cache[key].Value.Value;
+            }
+
+            return default(TValue);
+        }
+
+        public void Set(TKey key, TValue value)
+        {
+            if (ShouldEvict(key))
+                EvictLeastRecentlyAccessed();
+
+            if (cache.ContainsKey(key))
+                Remove(cache[key]);
+
+            var node = CreateNode(key, value);
+            accesses.AddFirst(node);
+            cache[key] = node;
+        }
+
+        private void SetMostRecentlyAccessed(TKey key)
+        {
+            var node = cache[key];
+            accesses.Remove(node);
+            accesses.AddFirst(node);
+        }
+
+        private bool ShouldEvict(TKey key)
+        {
+            return cache.Count == size && !cache.ContainsKey(key);
+        }
+
+        private void EvictLeastRecentlyAccessed()
+        {
+            var node = accesses.Last;
+            Remove(node);
+        }
+
+        private void Remove(LinkedListNode<KeyValuePair<TKey, TValue>> node)
+        {
+            cache.Remove(node.Value.Key);
+            accesses.Remove(node);
+        }
+
+        private LinkedListNode<KeyValuePair<TKey, TValue>> CreateNode(TKey key, TValue value)
+        {
+            return new LinkedListNode<KeyValuePair<TKey, TValue>>(new KeyValuePair<TKey, TValue>(key, value));
+        }
+
+        private int size;
+
+        private LinkedList<KeyValuePair<TKey, TValue>> accesses;
+
+        private Dictionary<TKey, LinkedListNode<KeyValuePair<TKey, TValue>>> cache;
+    }
+}
